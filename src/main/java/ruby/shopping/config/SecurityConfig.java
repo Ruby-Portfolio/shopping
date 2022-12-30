c9c15f4e93c6473364b45f03803a578c13f208b6
MisterRuby <ruby8700@naver.com>
2022-12-30 18:09:39 +0900

    feat : 모든 url 요청 허용으로 설정

---
+++ src/main/java/ruby/shopping/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package ruby.shopping.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        // 페이지 권한 설정
+        http
+                .authorizeHttpRequests()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
