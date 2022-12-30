8fac0fecfa32e574065b145c51c7389a64ddffb1
MisterRuby <ruby8700@naver.com>
2022-12-30 21:42:08 +0900

    feat : Swagger 설정

--- src/main/java/ruby/shopping/config/SecurityConfig.java
+++ src/main/java/ruby/shopping/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package ruby.shopping.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -19,8 +22,14 @@ public class SecurityConfig {
         // 페이지 권한 설정
         http
                 .authorizeHttpRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .anyRequest().permitAll();
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
