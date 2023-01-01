8a8d6dc3916a7602e905ad5954dbfb2be6f3b425
MisterRuby <ruby8700@naver.com>
2023-01-01 23:16:11 +0900

    test : 주문 목록 조회 단위 테스트
    회원의 주문 목록 및 주문 상품 목록 조회

--- src/main/java/ruby/shopping/domain/order/OrderRepositoryImpl.java
+++ src/main/java/ruby/shopping/domain/order/OrderRepositoryImpl.java
@@ -3,13 +3,12 @@ package ruby.shopping.domain.order;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import ruby.shopping.domain.account.Account;
-import ruby.shopping.domain.product.QProduct;
 
 import java.util.List;
 
 import static ruby.shopping.domain.order.QOrder.order;
 import static ruby.shopping.domain.orderProduct.QOrderProduct.orderProduct;
-import static ruby.shopping.domain.product.QProduct.*;
+import static ruby.shopping.domain.product.QProduct.product;
 
 @RequiredArgsConstructor
 public class OrderRepositoryImpl implements OrderRepositoryCustom{
---
+++ src/test/java/ruby/shopping/domain/order/OrderRepositoryTest.java
@@ -0,0 +1,121 @@
+package ruby.shopping.domain.order;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import ruby.shopping.TestQueryDslConfig;
+import ruby.shopping.domain.account.Account;
+import ruby.shopping.domain.account.AccountRepository;
+import ruby.shopping.domain.orderProduct.OrderProduct;
+import ruby.shopping.domain.orderProduct.OrderProductRepository;
+import ruby.shopping.domain.product.Product;
+import ruby.shopping.domain.product.ProductRepository;
+import ruby.shopping.domain.product.enums.Category;
+import ruby.shopping.domain.seller.Seller;
+import ruby.shopping.domain.seller.SellerRepository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import(TestQueryDslConfig.class)
+class OrderRepositoryTest {
+
+    @Autowired
+    OrderProductRepository orderProductRepository;
+    @Autowired
+    OrderRepository orderRepository;
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    SellerRepository sellerRepository;
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    EntityManager entityManager;
+
+    Account account;
+    Order order;
+    List<OrderProduct> orderProducts;
+
+    @BeforeEach
+    void before() {
+        String email = "test@gmail.com";
+        String password = "1234qwer!@";
+        account = Account.builder()
+                .email(email)
+                .password(password)
+                .build();
+        accountRepository.save(account);
+
+        Seller seller = Seller.builder()
+                .sellerName("김루비")
+                .accountNumber("013124512311")
+                .accountHolder("김예금주")
+                .account(account)
+                .build();
+        sellerRepository.save(seller);
+
+        Product product = Product.builder()
+                .name("플루트")
+                .price(10000000)
+                .description("관악기")
+                .category(Category.HOBBY)
+                .seller(seller)
+                .build();
+        productRepository.save(product);
+
+        order = Order.builder()
+                .account(account)
+                .build();
+        orderRepository.save(order);
+
+        OrderProduct orderProduct1 = OrderProduct.builder()
+                .product(product)
+                .count(1)
+                .order(order)
+                .build();
+        orderProductRepository.save(orderProduct1);
+        OrderProduct orderProduct2 = OrderProduct.builder()
+                .product(product)
+                .count(3)
+                .order(order)
+                .build();
+        orderProductRepository.save(orderProduct2);
+        orderProducts = List.of(orderProduct1, orderProduct2);
+
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+    @AfterEach
+    void after() {
+        orderProductRepository.deleteAll();
+        orderRepository.deleteAll();
+        productRepository.deleteAll();
+        sellerRepository.deleteAll();
+        accountRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("회원의 주문 목록 및 주문 상품 목록 조회")
+    void test_findByAccountFetchOrderProduct() {
+        List<Order> orders = orderRepository.findByAccountFetchOrderProduct(account);
+
+        boolean isOrderProducts = orders.stream()
+                .allMatch(order -> order.getOrderProducts().size() == orderProducts.size());
+
+        boolean isProducts = orders.stream()
+                .allMatch(order -> order.getOrderProducts().stream().allMatch(orderProduct -> orderProduct.getProduct() != null));
+
+        assertThat(orders.get(0).getId()).isEqualTo(order.getId());
+        assertThat(isOrderProducts).isTrue();
+        assertThat(isProducts).isTrue();
+    }
+}
