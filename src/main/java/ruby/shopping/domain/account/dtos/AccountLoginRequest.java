67f3c095b8bbb57da84c8bab7a6a3e03aa49cef1
MisterRuby <ruby8700@naver.com>
2023-01-02 14:08:38 +0900

    docs : 기능구현, API Docs 내용 정리

--- src/main/java/ruby/shopping/domain/account/dtos/AccountLoginRequest.java
+++ src/main/java/ruby/shopping/domain/account/dtos/AccountLoginRequest.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotBlank;
 @Setter
 public class AccountLoginRequest {
 
-    @Schema(description = "회원 이메일", example = "ruby0723@gmail.com")
+    @Schema(description = "회원 이메일", example = "rubykim0723@gmail.com")
     @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
     @Schema(description = "비밀번호", example = "qwer1234%")
